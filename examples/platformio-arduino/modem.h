3a37a0f7b81f2e92ed1e781c3ced4a37e8040cd8
RobertByrnes <robbyrnes@hotmail.co.uk>
2022-12-07 22:26:43 +0000

    refactor: remove ConfigParser from deps

---
+++ examples/platformio-arduino/modem.h
@@ -0,0 +1,7 @@
+#define TINY_GSM_MODEM_SIM800 // Modem is SIM800
+#define TINY_GSM_RX_BUFFER              1024 // Set RX buffer to 1Kb
+#define SIM800_SERIAL                   Serial1
+#define I2C_SDA                         21 // Serial data
+#define I2C_SCL                         22 // Serial clock
+#define IP5306_ADDR                     0x75 // Initiate communication
+#define IP5306_REG_SYS_CTL0             0x00 // Begin write
