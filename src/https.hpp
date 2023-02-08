54a4a86e7ba42570913d45dc862f7eee33b6bc55
Robert Byrnes <robbyrnes@hotmail.co.uk>
2023-02-08 16:45:56 +0000

    feat(HTTPS class): Add postJSON request support

--- src/https.hpp
+++ src/https.hpp
@@ -13,6 +13,8 @@ class HTTPS {
     public:
     const static int _httpsTimeout = 30000L;
     static std::string get(TinyGsm &sim_modem, HttpClient &http_client, const char * resource);
+    static std::string postJSON(TinyGsm &sim_modem, HttpClient &http_client, const char * endPoint, const char * requestBody, std::string currentToken);
+    static std::string print(TinyGsm &sim_modem, HttpClient &http_client, const char * requestBody);
     static bool download(TinyGsm &sim_modem, HttpClient &http_client, const char * resource, const char * filePath, uint32_t knownCRC32=0);
 
     private:
@@ -44,6 +46,52 @@ std::string HTTPS::get(TinyGsm &sim_modem, HttpClient &http_client, const char *
     } else {
         throw HTTPS_NO_GPRS_CONN;
     }
+}
+
+std::string HTTPS::postJSON(TinyGsm &sim_modem, HttpClient &http_client, const char * endPoint, std::string requestBody, std::string currentToken) {
+    http_client.setHttpResponseTimeout(HTTPS::_httpsTimeout);
+    http_client.connectionKeepAlive();
+    http_client.beginRequest();
+    http_client.post(endPoint);
+    http_client.sendHeader(HTTP_HEADER_CONTENT_TYPE, "application/json");
+    http_client.sendHeader(HTTP_HEADER_CONTENT_LENGTH, requestBody.length());
+    if (currentToken != "") {
+        std::string authHeader = "Bearer " + currentToken;
+        http_client.sendHeader("Authorization", authHeader.c_str());
+    }
+    http_client.endRequest();
+    http_client.write((const byte*)requestBody.c_str(), requestBody.length());
+    int status = http_client.responseStatusCode();
+}
+
+std::string HTTPS::print(TinyGsm &sim_modem, HttpClient &http_client, const char * requestBody) {
+    if (sim_modem.isGprsConnected()) {
+        log_i("Attempting to download");
+        http_client.print(requestBody);
+        int statusCode = http_client.responseStatusCode();
+        log_i("Status code: %i", statusCode);
+        if (statusCode == 200) {
+            std::string response;
+            std::string accessToken;
+            unsigned long timeoutStart = millis();
+            char c;
+
+            while (
+                (http_client.connected() || http_client.available()) &&
+                (!http_client.endOfBodyReached()) &&
+                ((millis() - timeoutStart) < HTTPS::_httpsTimeout)
+            ) {
+                c = http_client.read();
+                response += c;
+            }
+
+            return response;
+        } else {
+            throw HTTPS_NONE_200_RESP;
+        }
+    } else {
+        throw HTTPS_NO_GPRS_CONN;
+    }    
 }
 
 bool HTTPS::download(TinyGsm &sim_modem, HttpClient &http_client, const char * resource, const char * filePath, uint32_t knownCRC32) {
